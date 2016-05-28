dd2201087860c86944f087d15d5d61aad6b4e23e
fernandobd42 <fernandolucasbd@gmail.com>
2016-05-28 09:58:01 -0300

    Main

---
+++ src/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import style from './Style.css';
+
+var Header = () => <div style={{ backgroundColor: 'green', fontSize: 50, textAlign: 'center', width: '100%'}}> Header </div>
+export var Footer = () => <div className={style.main}> Footer </div>
+
+export default Header;
